4c78635f5e055f6890cc863f03d89aa4d3728dc1
xMurungi <murungij@gmail.com>
2024-09-12 17:28:47 +0300

    Add image size changes.

--- app/src/main/java/com/joses/scaffoldnav/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/joses/scaffoldnav/ui/screens/ProfileScreen.kt
@@ -1,15 +1,20 @@
 package com.joses.scaffoldnav.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.joses.scaffoldnav.R
@@ -23,7 +28,10 @@ fun ProfileScreen(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
-   LazyColumn {
+   LazyColumn(
+       horizontalAlignment = Alignment.CenterHorizontally,
+       verticalArrangement = Arrangement.Center
+   ) {
        item {
            UserDetails(
                modifier = modifier
@@ -38,10 +46,15 @@ fun UserDetails(
 ) {
     Column(
         modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Image(
             imageVector = ImageVector.vectorResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "User Image"
+            contentDescription = "User Image",
+            modifier = Modifier
+                .size(700.dp)
         )
         Text(
             text = "xJoses"
